8320a9123dbcb77efd09b9c72edee110ef305a4a
Raivo Pöhönen <raivo.gm@gmail.com>
2017-04-06 13:04:04 +0300

    Update templates
    * Fix CSV upload page;
    * remove boostrap 2 files.

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -1,20 +1,10 @@
 modules = {
     application {
-        resource url: 'js/application.js'
-        resource url: 'css/AlaBsAdditions.css'
-    }
-
-    fancybox {
-        resource url: 'js/fancybox3/jquery.fancybox.min.css'
-        resource url: 'js/fancybox3/jquery.fancybox.min.js'
+        resource url: [dir: 'js', file: 'application.js']
+        resource url: [dir: 'css', file: 'AlaBsAdditions.css'], disposition: 'head'
     }
 
     amplify {
         resource url: [dir: 'js', file: 'amplify.js']
-    }
-
-    fileupload {
-        resource url: [dir: 'js', file: 'bootstrap-fileupload.min.js']
-        resource url: [dir: 'css', file: 'bootstrap-fileupload.min.css']
     }
 }
